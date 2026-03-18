107e23484e3fd50ff1adcc1845ec4fc6aee3b18b
Sheryl Beula Martina <sherylbeula@gmail.com>
2026-03-18 10:42:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Flyweight Factory
 class IconFactory {
     constructor() {
         this.icons = {};
@@ -7,39 +6,73 @@ class IconFactory {
     getIcon(type) {
         if (!this.icons[type]) {
             let img = document.createElement("img");
-            img.src = this.getIconPath(type);
+            img.src = this.getPath(type);
+            img.classList.add("icon");
             this.icons[type] = img;
         }
         return this.icons[type];
     }
 
-    getIconPath(type) {
-        switch(type) {
-            case "star": return "https://cdn-icons-png.flaticon.com/512/1828/1828884.png";
-            case "heart": return "https://cdn-icons-png.flaticon.com/512/833/833472.png";
-            default: return "https://cdn-icons-png.flaticon.com/512/1828/1828884.png";
-        }
+    getPath(type) {
+        if (type === "star")
+            return "https://cdn-icons-png.flaticon.com/512/1828/1828884.png";
+        if (type === "heart")
+            return "https://cdn-icons-png.flaticon.com/512/833/833472.png";
     }
 }
 
 const factory = new IconFactory();
+let allIcons = [];
 
 function loadIcons() {
     const container = document.getElementById("iconContainer");
     container.innerHTML = "";
+    allIcons = [];
 
     let types = ["star", "heart"];
 
     for (let i = 0; i < 1000; i++) {
         let type = types[Math.floor(Math.random() * types.length)];
 
-        // Get shared icon (Flyweight)
         let icon = factory.getIcon(type);
-
-        // Clone for display (lightweight)
         let clone = icon.cloneNode(true);
-        clone.classList.add("icon");
 
-        container.appendChild(clone);
+        let card = document.createElement("div");
+        card.classList.add("icon-card");
+        card.setAttribute("data-type", type);
+
+        card.appendChild(clone);
+        container.appendChild(card);
+
+        allIcons.push(card);
     }
+
+    updateCount();
+}
+
+function filterIcons() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let filter = document.getElementById("filter").value;
+
+    let visible = 0;
+
+    allIcons.forEach(card => {
+        let type = card.getAttribute("data-type");
+
+        let matchSearch = type.includes(search);
+        let matchFilter = (filter === "all" || type === filter);
+
+        if (matchSearch && matchFilter) {
+            card.style.display = "block";
+            visible++;
+        } else {
+            card.style.display = "none";
+        }
+    });
+
+    document.getElementById("count").innerText = "Icons: " + visible;
+}
+
+function updateCount() {
+    document.getElementById("count").innerText = "Icons: " + allIcons.length;
 }
